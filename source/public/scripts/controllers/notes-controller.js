20356d22885d8adc58568b1c989eb5fa5ec87893
Daniel Blöchlinger <haven@hispeed.ch>
2024-06-03 20:36:39 +0200

    Open and close editNote added

--- source/public/scripts/controllers/notes-controller.js
+++ source/public/scripts/controllers/notes-controller.js
@@ -1,10 +1,14 @@
 import toolsForm from "./tools-form.js";
 import toolsTheme from "./tools-theme.js";
 
-const editNoteForm = document.querySelector(".editNote__form");
+const btnCancelClose = document.querySelector(".editNote__btnCancelClose");
+const btnNewNote = document.querySelector(".header__btnNewNote");
 const btnTheme = document.querySelectorAll(".header__btnTheme");
+const editNoteForm = document.querySelector(".editNote__form");
 
-editNoteForm.addEventListener("input", toolsForm.outputRange);
+btnCancelClose.addEventListener("click", toolsForm.cancelClose);
+btnNewNote.addEventListener("click", toolsForm.newNote);
 btnTheme.forEach((node) =>
     node.addEventListener("click", toolsTheme.changeTheme),
 );
+editNoteForm.addEventListener("input", toolsForm.outputRange);
--- source/public/scripts/controllers/tools-form.js
+++ source/public/scripts/controllers/tools-form.js
@@ -1,4 +1,21 @@
+const editNoteContainer = document.querySelector(".editNoteContainer");
+const notesAppContainer = document.querySelector(".notesAppContainer");
+
 const toolsForm = {
+    cancelClose: () => {
+        toolsForm.editNoteClose();
+    },
+    editNoteClose: () => {
+        editNoteContainer.classList.add("hidden");
+        notesAppContainer.classList.remove("hidden");
+    },
+    editNoteOpen: () => {
+        editNoteContainer.classList.remove("hidden");
+        notesAppContainer.classList.add("hidden");
+    },
+    newNote: () => {
+        toolsForm.editNoteOpen();
+    },
     outputRange: () => {
         const output = document.querySelector(".editNote__importanceOutput");
         const importance = document.querySelector(".editNote__importance");
