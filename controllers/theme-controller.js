aa23d30d476ae3830edf0f1aeb3dfef65a537645
Daniel Blöchlinger <haven@hispeed.ch>
2024-06-14 00:07:22 +0200

    Save theme with random id

--- controllers/theme-controller.js
+++ controllers/theme-controller.js
@@ -3,7 +3,18 @@ import toolsCheck from "./tools-check.js";
 
 const themeController = {
     showTheme: async (req, res) => {
-        res.send(await themeService.dbLoadTheme());
+        const data = await themeService.dbLoadTheme();
+        if (data === null) {
+            const theme = {
+                theme: 0,
+                type: "theme",
+            };
+            await themeService.dbSaveTheme(theme);
+            const newData = await themeService.dbLoadTheme();
+            res.send(newData);
+        } else {
+            res.send(data);
+        }
     },
     updateTheme: async (req, res) => {
         const { body } = req;
@@ -14,7 +25,7 @@ const themeController = {
         ) {
             res.sendStatus(400);
         } else {
-            await themeService.dbSaveTheme(body);
+            await themeService.dbUpdateTheme(body);
             res.send(body);
         }
     },
--- services/theme-service.js
+++ services/theme-service.js
@@ -1,16 +1,17 @@
 import { db } from "./datastore.js";
 
 const themeService = {
-    dbLoadTheme: async () => db.findOneAsync({ _id: "w5oyT7JohG0gBf1I" }),
-    dbSaveTheme: async (data) => {
+    dbLoadTheme: async () => db.findOneAsync({ type: "theme" }),
+    dbSaveTheme: async (theme) => {
         try {
-            await db.updateAsync({ _id: "w5oyT7JohG0gBf1I" }, data, {
-                upsert: true,
-            });
+            await db.insertAsync(theme);
         } catch (error) {
             // eslint-disable-next-line no-console
             console.log(error);
         }
+    },
+    dbUpdateTheme: async (data) => {
+        await db.updateAsync({ type: "theme" }, { $set: data }, {});
     },
 };
 
